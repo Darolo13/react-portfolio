bb04586f08c95f0ee5cc246fca429423e63d0cdc
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-16 16:20:49 -0700

    Topbar & line animation

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import Contact from './components/Contact'
 // style 
 import './app.scss'
 
+import { useState } from 'react';
+
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true)
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
