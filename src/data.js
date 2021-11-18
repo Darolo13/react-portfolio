fc7f7a635e1820ae6a8309edb11ce0745f3aca2c
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-18 15:38:15 -0700

    Portfolio section

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "BytePay",
+        img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
+    }
+]
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "BytePay",
+        img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
+    }
+]
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "BytePay",
+        img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
+    }
+]
+
+export const designPortfolio = [
+    {
+        id: 1,
+        title: "BytePay",
+        img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
+    }
+]
+
+export const contentPortfolio = [
+    {
+        id: 1,
+        title: "BytePay",
+        img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
+    }
+]
