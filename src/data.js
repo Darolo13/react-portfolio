d5d4dcd45214f253ddf34581472eec6873d3aaea
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-18 22:49:18 -0700

    Add dev-wiki to web projects

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ export const featuredPortfolio = [
     {
         id: 1,
         title: "BytePay",
+        alt: "Coming Soon!",
         img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
     },
     {
@@ -16,7 +17,14 @@ export const webPortfolio = [
     {
         id: 1,
         title: "BytePay",
+        alt: "Coming Soon!",
         img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
+    },
+    {
+        id: 2,
+        title: "Dev-wiki",
+        img: "https://github.com/zdensamson/DEV-WIKI/raw/main/assets/screenshot/main.png",
+        link: "https://dev-wiki-io.herokuapp.com/"
     }
 ]
 
@@ -24,6 +32,7 @@ export const mobilePortfolio = [
     {
         id: 1,
         title: "BytePay",
+        alt: "Coming Soon!",
         img: "https://cdn.pixabay.com/photo/2019/12/27/08/36/coming-soon-hour-glass-4721933_960_720.png"
     }
 ]
