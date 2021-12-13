1c011fc2bd7175d326ea73c4b9e5507eb267f391
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-12-13 16:12:35 -0700

    Add Resume and Technical Skills

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -5,16 +5,18 @@ import resume from '../../assets/resume.pdf';
 export default function Resume() {
     return (
         <div className="resume" id="resume">
-            <div className="container">
+            <h2>About Me</h2>
+            <div className="left">
+
                 <div className="summary">
                     <h2>Resume</h2>
                     <p>Hello, I'm a Full Stack Developer with knowledge in MERN Stack and responsive web development. Recently,
                         I Attended the University of Arizona Coding Bootcamp where I received a Certifcate in Full Stack Web Development.
-                        Where I learned best practices for designing powerful mobile-friendly websites and applications.
-                        I currently work part-time as an IT Technician for a small real-estate business in Chapel Hill.</p>
+                        Where I learned best practices for designing powerful mobile-friendly websites and applications.</p>
                 </div>
             </div>
-            <div className="techSkills">
+            <div className="right">
+            <div className="tech-skills">
                 <h4>Technical Skills</h4>
                 <ul>
                     <li><b>Programming Languages:</b> HTML, CSS, SASS, JavaScript</li>
@@ -22,10 +24,12 @@ export default function Resume() {
                     <li><b>Library / Frameworks:</b> React.js, jQuery, Bootstrap, Jest </li>
                     <li><b>Other Tech:</b> Node.js, Handlebars.js, Git </li>
                 </ul>
-                <a href={resume} target='_blank' rel="noopener noreferrer" className="resumeLink"><button className="btn"
-                    id="aboutResumeBtn" type="submit">Download my resume:
-                    <i className="fa fa-download resume"></i></button></a>
+                <a href={resume} target='_blank' rel="noopener noreferrer" className="resumeLink">
+                    <button>Download my resume <i class="fas fa-download"></i>
+                    </button>
+                </a>
             </div>
+                        </div>
         </div>
     )
 }
